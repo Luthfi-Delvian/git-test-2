db7bad7e650685ed5dc2d1fe99ea727764958e32
Luthfi-Delvian <gfactoryld03@gmail.com>
2021-04-07 08:47:19 +0700

    menambahkan isi index.html

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test git remote</title>
+</head>
+
+<body>
+    <h1>Hello World !</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum consequuntur corporis eum odio odit sunt quam
+        eligendi quos harum provident nam, consectetur, iste dolores earum dolorem dolor reiciendis quasi porro!</p>
+</body>
+
+</html>
