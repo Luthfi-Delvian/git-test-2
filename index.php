10aa49ebc6eebebff957f64237e119f8abaaed05
Luthfi-Delvian <gfactoryld03@gmail.com>
2021-04-07 08:54:30 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -12,6 +12,10 @@
     <h1>Hello World !</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum consequuntur corporis eum odio odit sunt quam
         eligendi quos harum provident nam, consectetur, iste dolores earum dolorem dolor reiciendis quasi porro!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
